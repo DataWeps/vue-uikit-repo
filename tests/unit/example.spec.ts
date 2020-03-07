0468bc03fd611adb7fd5b7460cee1ba9040c1e3d
MartinKravec <kravec.martin@gmail.com>
2020-03-07 12:05:49 +0100

    Refactor unit test

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,11 +1,11 @@
-import { shallowMount, mount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 import TestComponent from '@/components/TestComponent.vue'
 
 describe('TestComponent.vue', () => {
   it('renders "Test component"', () => {
     const msg = 'Test component'
 
-    const wrapper = mount(TestComponent)
+    const wrapper = shallowMount(TestComponent)
     expect(wrapper.find('h1').exists()).toBe(true)
     expect(wrapper.find('h1').text()).toBe(msg)
   })
